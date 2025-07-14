9281c0de18dd95b3405899633ebd852ce5df30e5
Daniel Suzuki <53331046+dasuzuki1@users.noreply.github.com>
2025-07-14 14:40:37 +0000

    fixed stuff

--- terraform-code/modules/repos/main.tf
+++ terraform-code/modules/repos/main.tf
@@ -23,11 +23,11 @@ module "deploy-key" {
   repo_name = each.key
 }
 
-module "info-page" {
+/* module "info-page" {
   source           = "./modules/info-page"
   repos            = { for k, v in module.repos["prod"].clone-urls : k => v }
   run_provisioners = false
-}
+} */
 
 output "repo-info" {
   value = { for k, v in module.repos : k => v.clone-urls }
